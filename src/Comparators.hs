51d0d7441965c7b0bb62b22b9458072b45441c5a
cliuj <jliu2179@gmail.com>
2021-11-14 21:11:36 -0500

    refactor: Clean up weighted euclidean distance comparator

--- src/Comparators.hs
+++ src/Comparators.hs
@@ -14,12 +14,12 @@ euclideanDistance a b = sqrt . fromIntegral $ sum $ map square $ zipWith (-) a b
 -- Taken from https://www.compuphase.com/cmetric.htm
 weightedEuclideanDistance :: [Int] -> [Int] -> Float
 weightedEuclideanDistance a b = do
-    let r1 = fromIntegral $ head a :: Float
-    let r2 = fromIntegral $ head b :: Float
     let r = (r1 + r2) / 2
-    let dist = map square $ zipWith (-) a b
+            where r1 = fromIntegral $ head a :: Float
+                  r2 = fromIntegral $ head a :: Float
     let weightG = 4.0
     let weightR = 2 + r / 256
     let weightB = 2 + ((255 - r) / 256)
     sqrt $ weightR * fromIntegral (head dist) + weightG * fromIntegral (dist !! 1) + weightB * fromIntegral (last dist)
+            where dist = map square $ zipWith (-) a b
 
