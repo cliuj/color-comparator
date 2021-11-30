2c34e10a849beebc9bd45078e2a495bc2398556c
cliuj <jliu2179@gmail.com>
2021-11-29 23:49:13 -0500

    feat: Add --file argument to allow alternative color comparison source
    This commit adds a --file flag to the CLI arguments to allow comparison
    of a hex color string to a JSON file of colors. The format of the JSON
    file is based off of the term_256_colors.json file.
    
    New additional changes includes:
    * CLI parsing using optparse-applicative
      - This will be the CLI library used going forward for future CLI features
    * Color record type is restructured to Term256Color record's with Maybe
      types for optional fields
      - Term256Color record struct is removed.
      - This was done to keep consistency between different file inputs and
        makes it easier to maintain.

--- src/ResultBuilder.hs
+++ src/ResultBuilder.hs
@@ -5,9 +5,17 @@ module ResultBuilder
     , displayTerm256Color
     , displayRgbColor
     , buildResult
+    , printResult
     ) where
 
 import Text.Printf
+import Data.Map ((!))
+import Data.Maybe (fromMaybe)
+
+import Colors (RGB(..), rgbToList, IdMap)
+
+notTermID :: Int
+notTermID = -1
 
 term256EscStr :: String
 term256EscStr = "\ESC[38;5;%dm███████\ESC[0m "
@@ -18,26 +26,32 @@ termRgbEscStr = "\ESC[38;2;%d;%d;%dm███████\ESC[0m "
 -- Result is the base result output returned. Additional outputs can be appended/prepended
 -- to the Result output string.
 data Result = Result
-              { resultHex :: String
-              , resultRgb :: [Int]
-              , resultDistance :: Float
+              { hexString :: String
+              , rgb :: RGB
+              , distance :: Float
               } deriving (Show)
 
 resultToString :: Result -> String
-resultToString result = printf "%s %s %s" hex rgb distance
+resultToString result = printf "%s %s %s" hexString' rgb' distance'
     where
-        hex = resultHex result
-        rgb = show $ resultRgb result
-        distance = show $ resultDistance result
+        hexString' = hexString result
+        rgb' = show $ rgbToList $ rgb result
+        distance' = show $ distance result
 
 displayTerm256Color :: Int -> String
 displayTerm256Color = printf term256EscStr
 
-displayRgbColor :: [Int] -> String
-displayRgbColor rgb = printf termRgbEscStr r g b
-    where r = head rgb
-          g = rgb !! 1
-          b = rgb !! 2
+displayRgbColor :: RGB -> String
+displayRgbColor rgb = printf termRgbEscStr (r rgb) (g rgb) (b rgb)
 
 buildResult :: String -> [String]-> String
 buildResult r s = unwords s ++ " " ++ r ++ "\n"
+
+printResult :: Result -> Maybe IdMap -> IO ()
+printResult r (Just idMap) = putStr $ buildResult (resultToString r) [ displayColor, termID ]
+    where
+        displayColor = if id == notTermID then "" else displayTerm256Color id
+        termID = if id == notTermID then "" else show id
+        id = fromMaybe notTermID (idMap ! hexString r)
+
+printResult r Nothing = putStr $ buildResult (resultToString r) [ displayRgbColor $ (rgb :: Result -> RGB) r ]
